8aa4ead5a0d88ca0e9749323dbde2d0a11fc7d25
Firstpioneer <1759736807@qq.com>
2025-04-19 18:58:46 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://Firstpioneer.github.io",
+  base: "/astro-blog",
   trailingSlash: "always",
   integrations: [
     tailwind(
